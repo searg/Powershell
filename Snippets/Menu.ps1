16fe062b9b8ff266e218e9acf742b79ab19175bf
Searg <pod.serg@gmail.com>
2017-08-04 14:08:49 +0300

    Add snippet "Menu"

---
+++ Snippets/Menu.ps1
@@ -0,0 +1,27 @@
+﻿# Код ниже показывает как реализовать меню
+# Доступные варианты да (Yes) и нет (No)
+# Другие варианты не принимаются
+# http://technet.microsoft.com/en-us/library/ff730939.aspx
+
+$Title = "Запрос подтверждения"
+$Message = "Ты действительно хочешь сделать это?"
+
+$Yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+    "Да, хочу."
+$No = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+    "Нет, не хочу."
+
+$Options = [System.Management.Automation.Host.ChoiceDescription[]]($Yes, $No)
+$Result = $host.ui.PromptForChoice($Title, $Message, $Options, 0) 
+
+switch ($Result)
+    {
+        # Действия, если "Yes"
+        0	{
+				Write-Host -ForegroundColor Green "OK! Мы сделали это!"
+			}
+        # Действия, если "No"
+        1	{
+				Write-Host -ForegroundColor Red "Нет, так нет"
+			}
+    }
