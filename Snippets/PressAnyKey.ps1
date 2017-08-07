1e943028c389d97b95a5c100bfff4134072a6741
Searg <pod.serg@gmail.com>
2017-08-07 16:31:46 +0300

    Add "PressAnyKey" snippet

---
+++ Snippets/PressAnyKey.ps1
@@ -0,0 +1,7 @@
+﻿# Пример, демонстрирующий как приостановить работу скрипта/функции
+# до нажатия любой клавиши
+
+Write-Host 'Press any key to continue...'
+$null = [Console]::ReadKey('?')
+# или так:
+# $null = $host.UI.RawUI.ReadKey()
